665a2fc0d633181b329335de5626f8af4294870f
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-06-20 23:55:42 -0300

    impedir consultas desnecessárias com o useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -4,21 +4,25 @@ import { useSearchParams } from 'react-router-dom'
 import { PessoasSesvices } from '../../shared/services/api/pessoas/PessoasSesvices'
 import { FerramentasDaListagem } from '../../shared/components'
 import { LayoutBaseDePagina } from '../../shared/layouts'
+import { useDebounce } from '../../shared/hooks'
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
+  const { debounce } = useDebounce()
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
   }, [searchParams])
 
   useEffect(() => {
-    PessoasSesvices.getAll(1, busca).then(result => {
-      if (result instanceof Error) {
-        alert(result.message)
-      } else {
-        console.log(result.data[0].nomeCompleto)
-      }
+    debounce(() => {
+      PessoasSesvices.getAll(1, busca).then(result => {
+        if (result instanceof Error) {
+          alert(result.message)
+        } else {
+          console.log(result)
+        }
+      })
     })
   }, [busca])
 
---
+++ src/shared/hooks/UseDebounce.ts
@@ -0,0 +1,15 @@
+import { useCallback, useRef } from 'react'
+
+export const useDebounce = () => {
+  const debouncing = useRef<NodeJS.Timeout>()
+
+  const debounce = useCallback((func: () => void) => {
+    
+    if(debouncing.current){
+        clearTimeout(debouncing.current)
+    }
+
+    debouncing.current = setTimeout(() => func(), 1000)
+  }, [])
+  return { debounce }
+}
