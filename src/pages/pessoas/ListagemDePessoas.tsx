6ca09a1d17fb4717f9a813410476ab06dffda870
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-06-20 22:49:50 -0300

    Consultando o backend e montando listagem no React

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,18 +1,30 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useSearchParams } from 'react-router-dom'
+
+import { PessoasSesvices } from '../../shared/services/api/pessoas/PessoasSesvices'
 import { FerramentasDaListagem } from '../../shared/components'
 import { LayoutBaseDePagina } from '../../shared/layouts'
 
-export const ListagemDeCidade: React.FC = () => {
+export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
   }, [searchParams])
 
+  useEffect(() => {
+    PessoasSesvices.getAll(1, busca).then(result => {
+      if (result instanceof Error) {
+        alert(result.message)
+      } else {
+        console.log(result.data[0].nomeCompleto)
+      }
+    })
+  }, [busca])
+
   return (
     <LayoutBaseDePagina
-      titulo="Listagem de cidades"
+      titulo="Listagem de pessoas"
       barraDeFerramentas={
         <FerramentasDaListagem
           textoBotaoNovo="Nova"
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { Dashboard, ListagemDeCidade } from '../pages'
+import { Dashboard, ListagemDePessoas } from '../pages'
 import { useDrawerContext } from '../shared/contexts'
 
 export const AppRoutes = () => {
@@ -15,8 +15,8 @@ export const AppRoutes = () => {
       },
       {
         icon: 'location_city',
-        path: '/cidades',
-        label: 'Cidades',
+        path: '/pessoas',
+        label: 'Pessoas',
       },
     ])
   }, [])
@@ -25,8 +25,8 @@ export const AppRoutes = () => {
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard />} />
 
-      <Route path="/cidades" element={<ListagemDeCidade />} />
-      {/* <Route path="/cidades/detalhe/:id" element={<Dashboard />} /> */}
+      <Route path="/pessoas" element={<ListagemDePessoas />} />
+      {/* <Route path="/pessoas/detalhe/:id" element={<Dashboard />} /> */}
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
